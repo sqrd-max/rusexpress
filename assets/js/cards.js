90481f3d78c9f09a6e439f11609d4b359c3c4f29
sqrd-max <sqrd-max@yandex.ru>
2021-04-14 01:10:47 +0300

    анимация

---
+++ assets/js/cards.js
@@ -0,0 +1,23 @@
+function showCard(e){
+    $('.section_loaded').addClass('section_hidden').removeClass('section_loaded');
+    $('.clicked').removeClass('clicked').addClass('noneclicked');
+    let target= e.target;
+    let currentCard=$(target).prop("id").split('#');
+    cardName=currentCard[0];
+    toShowCard = $('.'+cardName);
+    console.log(currentCard);
+    toShowCard.removeClass('section_hidden').addClass('section_loaded');
+    $('#'+cardName).removeClass('noneclicked').addClass('clicked');
+}
+$('#main__cooperation-cards-1').on("click",function(e){
+    showCard(e);
+});
+$('#main__cooperation-cards-2').on("click",function(e){
+    showCard(e);
+});
+$('#main__cooperation-cards-3').on("click",function(e){
+    showCard(e);
+});
+$('#main__cooperation-cards-4').on("click",function(e){
+    showCard(e);
+});
